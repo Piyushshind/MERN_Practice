dcf634f5b3a8b9a684aab8f01d09b25d9dbc1b50
Piyushshind <piyushshinde.199@gmail.com>
2025-08-30 10:38:50 +0530

    jwt

--- MernPractice/Server/utils/jwt.js
+++ MernPractice/Server/utils/jwt.js
@@ -4,3 +4,4 @@ const signAccessToken = (payload) =>
   jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: process.env.JWT_ACCESS_EXPIRES || '15m' });
 
 module.exports = { signAccessToken };
+
